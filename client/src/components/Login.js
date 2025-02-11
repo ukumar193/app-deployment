a5149c27d5d0cbdb1bbdea6fffb7cd75c5cb3380
u kumra <uppariukumar@gmail.com>
2025-02-11 15:21:51 +0530

    6 change

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -10,7 +10,7 @@ function Login() {
   useEffect(() => {
   
     if (localStorage.getItem("token")) {
-       onValidToken();
+      //  onValidToken();
     }
   }, []);
   let onValidToken = async () => {
